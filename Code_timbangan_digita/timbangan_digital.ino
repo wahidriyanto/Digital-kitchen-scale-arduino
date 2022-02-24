855f77ad04411876fd27d8a7f5f7710a857dadf2
Wahid riyanto <77404031+wahidriyanto@users.noreply.github.com>
2022-02-24 09:20:05 +0700

    Add files via upload
    In this project using a 10kg load cell, the accuracy is not good
    project maker : wahidriyanto

---
+++ Code_timbangan_digita/timbangan_digital.ino
@@ -0,0 +1,115 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define DT A0
+#define SCK A1
+#define sw 2
+
+long sample=0;
+float val=0;
+long count=0;
+
+unsigned long readCount(void)
+{
+  unsigned long Count;
+  unsigned char i;
+  pinMode(DT, OUTPUT);
+  digitalWrite(DT,HIGH);
+  digitalWrite(SCK,LOW);
+  Count=0;
+  pinMode(DT, INPUT);
+  while(digitalRead(DT));
+  for (i=0;i<24;i++)
+  {
+    digitalWrite(SCK,HIGH);
+    Count=Count<<1;
+    digitalWrite(SCK,LOW);
+    if(digitalRead(DT)) 
+    Count++;
+  }
+  digitalWrite(SCK,HIGH);
+  Count=Count^0x800000;
+  digitalWrite(SCK,LOW);
+  return(Count);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(SCK, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
+ 
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("    Weight ");
+  lcd.setCursor(0,1);
+  lcd.print(" Measurement ");
+  delay(100);
+  lcd.clear();
+  calibrate();
+}
+
+void loop()
+{
+  count= readCount();
+  int w=(((count-sample)/val)-2*((count-sample)/val));
+  Serial.print("weight:");
+  Serial.print((int)w);
+  Serial.println("g");
+  lcd.setCursor(0,0);
+  lcd.print("Weight            ");
+  lcd.setCursor(0,1);
+  lcd.print(w);
+  lcd.print("g             ");
+  delay (1000);
+
+  if(digitalRead(sw)==0)
+  {
+    val=0;
+    sample=0;
+    w=0;
+    count=0;
+    calibrate();
+  }
+}
+
+void calibrate()
+{
+    lcd.clear();
+  lcd.print("Calibrating...");
+  lcd.setCursor(0,1);
+  lcd.print("Please Wait...");
+  for(int i=0;i<100;i++)
+  {
+    count=readCount();
+    sample+=count;
+    Serial.println(count);
+  }
+  sample/=100;
+  Serial.print("Avg:");
+  Serial.println(sample);
+  lcd.clear();
+  lcd.print("Put 100g & wait");
+  count=0;
+  while(count<1000)
+  {
+    count=readCount();
+    count=sample-count;
+    Serial.println(count);
+  }
+  lcd.clear();
+  lcd.print("Please Wait....");
+  delay(200);
+  for(int i=0;i<100;i++)
+  {
+    count=readCount();
+    val+=sample-count;
+    Serial.println(sample-count);
+  }
+  val=val/100.0;
+  val=val/100.0;        // put here your calibrating weight
+  lcd.clear();
+}
